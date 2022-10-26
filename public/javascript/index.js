af2605cac4495d88b86de9eda9d087f7fae8d40a
David Hybner <dah1998@yahoo.com>
2022-10-26 03:23:20 -0400

    moved loanshark page
    updated links to page after move

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -3,7 +3,7 @@ const sharkbtn =document.querySelector('#start');
 sharkbtn.addEventListener('click', function() {
    sharkbtn.classList.add("animate__animated", "animate__hinge"); 
    let tID = setTimeout(function () {
-    window.location.href = "./LoanShark.html";
+    window.location.href = "./public/LoanShark.html";
     window.clearTimeout(tID);		// clear time out.
 }, 2000);
 })
