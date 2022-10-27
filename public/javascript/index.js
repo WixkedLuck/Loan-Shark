d32a761d8ad8c4c0b5edfdaad904848b85cc5c8d
David Hybner <dah1998@yahoo.com>
2022-10-27 19:02:03 -0400

    Update index.js
    fixed a bug when deploying to GitHub and refreshing the page. button would no longer be on the page due to adding an animation class

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -4,6 +4,8 @@ sharkbtn.addEventListener('click', function() {
    sharkbtn.classList.add("animate__animated", "animate__hinge"); 
    let tID = setTimeout(function () {
     window.location.href = "./public/LoanShark.html";
+    sharkbtn.classList.remove("animate__animated", "animate__hinge"); 
     window.clearTimeout(tID);		// clear time out.
 }, 2000);
+
 })
