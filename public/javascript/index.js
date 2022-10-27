bd785fbe3a9f392846d78a62d1a40efb2e4aef2f
David Hybner <dah1998@yahoo.com>
2022-10-27 19:10:33 -0400

    Update index.js
    changed to window.assign

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -3,9 +3,10 @@ const sharkbtn =document.querySelector('#start');
 sharkbtn.addEventListener('click', function() {
    sharkbtn.classList.add("animate__animated", "animate__hinge"); 
    let tID = setTimeout(function () {
-    window.location.href = "./public/LoanShark.html";
     sharkbtn.classList.remove("animate__animated", "animate__hinge"); 
-    window.clearTimeout(tID);		// clear time out.
+    window.clearTimeout(tID);
+    window.location.assign( "./public/LoanShark.html");
+   		// clear time out.
 }, 2000);
 
 })
