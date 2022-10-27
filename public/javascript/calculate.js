a52df1d3cf51f057a48138918db08abefdc5763e
David Hybner <dah1998@yahoo.com>
2022-10-27 12:55:09 -0400

    update js and reflect html
    created the math behind the calculations needed and displayed them to the page. updated readme with photo. added comments where needed.

--- public/javascript/calculate.js
+++ public/javascript/calculate.js
@@ -1,7 +1,7 @@
 const calcBtn =document.getElementById("submit");
-const Loan =document.getElementById("loan").value.trim();
-const interst=document.getElementById("interest").value.trim();
-const term=document.getElementById("term").value.trim();
+const Loan =document.getElementById("loan");
+const interst2=document.getElementById("interest");
+const term=document.getElementById("term");
 
 // on hover button
 calcBtn.addEventListener('mouseover', function(){
@@ -10,24 +10,25 @@ calcBtn.addEventListener('mouseover', function(){
     return new Promise(resolve => setTimeout(resolve, time));
 
 }
+// remove class after settime
 delay(1000).then(() => calcBtn.classList.remove('animate__animated', 'animate__pulse'));
 
 });
 calcBtn.addEventListener('click', function() {
-    console.log("clicked");
- 
-if(Loan=="" | interst=="" | term=="")
-{
-   window.alert("Please fill out the complete form");
-}
-else{
-CalculateMortgage(Loan,interst,term);
-}
+    // check to see if the full form is filled out
+ if(Loan.value=="" | interst2.value=="" | term.value=="")
+ {
+    window.alert("Please fill out the complete form");
+ }
+ else{
+
+CalculateMortgage(Loan,interst2,term);
+ }
 });
 
-function CalculateMortgage(loan,interest,term) {  
-    let principal = parseFloat(loan.value);
-    let cInterest = parseFloat(interest.value) / 100/ 12;
+function CalculateMortgage(Loan,interest2,term) {  
+    let principal = parseFloat(Loan.value);
+    let cInterest = parseFloat(interest2.value) / 100/ 12;
     let cPayments = parseFloat(term.value);
 
     let x = Math.pow(1+cInterest, cPayments);
@@ -35,6 +36,7 @@ function CalculateMortgage(loan,interest,term) {
     let total_payments = (monthly * cPayments).toFixed(2);
     let total_interest_sum = ((monthly * cPayments)-principal).toFixed(2);
 
+    // define currency type
     let formatCurrency = new Intl.NumberFormat(undefined, {
         style: 'currency',
         currency: 'USD'
@@ -43,7 +45,76 @@ function CalculateMortgage(loan,interest,term) {
     let fprincipal = formatCurrency.format(principal);
     let ftotal_payments = formatCurrency.format(total_payments);
     let ftotal_interest_sum = formatCurrency.format(total_interest_sum);
-
-
+    let info="";
+    // add info to table
+    info += "<table class='table table-striped bg-light table-sm'>";
     
+    info += "<tr><th colspan='2' style='text-align:center;'><h2>"+fmonthly+"</h2></th></tr>";
+
+    info += "<tr><td>Loan Amount:</td>";
+    info += "<td align='right'>"+fprincipal+"</td></tr>";
+
+    info += "<tr><td>Total Payments:</td>";
+    info += "<td align='right'>"+ftotal_payments+"</td></tr>";
+
+    info += "<tr><td>Total Interest Paid:</td>";
+    info += "<td align='right'>"+ftotal_interest_sum+"</td></tr>";
+
+    info += "<tr><td>Payments:</td>";
+    info += "<td align='right'>"+cPayments+"</td></tr>";
+
+    info += "</table>";
+
+    // display info to html
+    document.getElementById("loan_info").innerHTML = info;
+
+    // reset table
+    let table="";
+
+    table += "<table class='table table-striped bg-light table-sm'>";
+
+        table += "<tr>";
+        table += " <td width='30'><strong>Month</strong></td>";
+        table += "<td width='30'><strong>Payment</strong></td>";
+        table += "<td width='30'><strong>Principle</strong></td>";
+        table += " <td width='30'><strong>Interest</strong></td>";
+        table += "<td width='30'><strong>Interest Paid</strong></td>";
+        table += "<td width='30'><strong>Balance</strong></td>";
+        table += "</tr>";
+    // get input values
+    let current_balance = parseFloat(document.getElementById('loan').value);
+    let monthly_payment = monthly
+    // init counter
+    let payment_counter = 1;
+    let total_interest = parseFloat(cInterest);
+    while (payment_counter <= cPayments) {
+        towards_interest = cInterest * current_balance;
+        towards_balance = monthly_payment - towards_interest;
+        total_interest = total_interest + towards_interest; 
+        current_balance = current_balance - towards_balance;
+
+        let ftowards_interest = formatCurrency.format(towards_interest);
+        let ftowards_balance = formatCurrency.format(towards_balance);
+        let ftotal_interest = formatCurrency.format(total_interest);
+        let fcurrent_balance = formatCurrency.format(current_balance);
+        let fmonthly_payment = formatCurrency.format(monthly_payment);
+        
+        
+        table += "<tr>";
+            table += "<td>"+payment_counter+"</td>"
+            table += "<td>"+fmonthly_payment+"</td>"
+            table += "<td>"+ftowards_balance+"</td>"
+            table += "<td>"+ftowards_interest+"</td>"
+            table += "<td>"+ftotal_interest+"</td>"
+            table += "<td>"+fcurrent_balance+"</td>"
+        table += "</tr>";
+        payment_counter++;
+ 
+    }
+    table +="</table>";
+
+    document.getElementById("Schedule").innerHTML = table;
 }
+
+
+
